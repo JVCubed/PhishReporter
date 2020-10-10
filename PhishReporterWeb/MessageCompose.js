23001ab918958b026bb235aadf1273d38eb4156d
JVCubed <JVCubed@outlook.com>
2020-10-10 12:31:46 -0700

    Add project files.

---
+++ PhishReporterWeb/MessageCompose.js
@@ -0,0 +1,36 @@
+﻿'use strict';
+
+(function () {
+
+    Office.onReady(function () {
+        // Office is ready
+        $(document).ready(function () {
+            // The document is ready
+            getPhishingItem(Office.context.mailbox.item);
+            composeMail();
+        });
+    });
+
+    // defining global variables to pass them to the composeMail function
+    var phishItemId;
+    var phishSubject
+
+    // this function has to run first because when the new message form pops up the current iteminformation is lost. 
+    function getPhishingItem(item) {
+        phishItemId = item.itemId
+        phishSubject = item.subject
+    }
+
+    function composeMail() {
+        Office.context.mailbox.displayNewMessageForm({
+            toRecipients: ["Jeroen.van.der.Velden2@hva.nl"],
+            // ccRecipients: ["sam@contoso.com"], Send to more mailaddresses if nessecery
+            subject: Office.context.mailbox.userProfile.displayName + " is reporting phishing!",
+            htmlBody: 'Hello <b>phishing</b>!<br/></i>' + phishItemId,
+            attachments: [
+                { type: "item", itemId: phishItemId, name: phishSubject }
+            ],
+        });
+    }
+
+})();
---
+++ PhishReporterWeb/MessageRead.js
@@ -0,0 +1,50 @@
+﻿'use strict';
+
+(function () {
+
+    Office.onReady(function () {
+        // Office is ready
+        $(document).ready(function () {
+            // The document is ready
+            loadItemProps(Office.context.mailbox.item);
+            
+            composeMail();
+        });
+    });
+
+    
+
+    function loadItemProps(item) {
+        // Write message property values to the task pane
+        $('#item-id').text(item.itemId);
+        $('#item-subject').text(item.subject);
+        $('#item-internetMessageId').text(item.internetMessageId);
+        $('#item-from').html(item.from.displayName + " &lt;" + item.from.emailAddress + "&gt;");
+
+        
+
+        
+
+        //$('#debug').text(item.itemClass);
+
+       
+
+        //if (item.displayNewMessageForm !== undefined) {
+        //    $('#debug').text("displayNewMessageForm can be used.");
+            // Use item.displayNewMessageForm.
+        //    item.displayNewMessageForm;
+        //} else {
+        //    $('#debug').text("displayNewMessageForm can't be used.");
+        //}
+    }
+
+    function composeMail() {
+        Office.context.mailbox.displayNewMessageForm({
+            toRecipients: Office.context.mailbox.item.to, // Copies the To line from current item
+            ccRecipients: ["sam@contoso.com"],
+            subject: "Outlook add-ins are cool!",
+            htmlBody: 'Hello <b>World</b>!<br/><img src="cid:image.png"></i>',
+        });
+    }
+
+})();
