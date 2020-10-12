100c08ad44679df7a9945854627724bd0cb877ac
JVCubed <JVCubed@outlook.com>
2020-10-12 13:41:53 -0700

    Beta release ready

--- PhishReporterWeb/MessageCompose.js
+++ PhishReporterWeb/MessageCompose.js
@@ -6,6 +6,7 @@
         // Office is ready
         $(document).ready(function () {
             // The document is ready
+            securityTeamMailAddress();
             getPhishingItem(Office.context.mailbox.item);
             composeMail();
         });
@@ -13,20 +14,50 @@
 
     // defining global variables to pass them to the composeMail function
     var phishItemId;
-    var phishSubject
+    var phishSubject;
+    var securityTeamMailAddress;
 
-    // this function has to run first because when the new message form pops up the current iteminformation is lost. 
+    // check if there is an email address set to send the mail to
+    function securityTeamMailAddress() {
+        // check if email is already set
+        if (Office.context.roamingSettings.get("email")) {
+            securityTeamMailAddress = Office.context.roamingSettings.get("email")
+        }
+        // show popup to enter the email address to report phishing to.
+        else {
+            // TODO: Create popup to enter email at first run
+
+            // Office.context.roamingSettings.set("email", "j.vdvelden99@gmail.com")
+            securityTeamMailAddress = Office.context.roamingSettings.get("email")
+            saveRoamingSettings()
+        }   
+    }
+
+    // save value's to roaming settings so it can be accessed later
+    function saveRoamingSettings() {
+        // Save settings in the mailbox to make it available in future sessions.
+        Office.context.roamingSettings.saveAsync(function (result) {
+            if (result.status !== Office.AsyncResultStatus.Succeeded) {
+                console.error(`Action failed with message ${result.error.message}`);
+            } else {
+                console.log(`Settings saved with status: ${result.status}`);
+            }
+        });
+    }
+
+    // this function has to run before composing a new mail to retrieve the details of the current selected email. 
     function getPhishingItem(item) {
         phishItemId = item.itemId
         phishSubject = item.subject
     }
 
+    // function to open a new 'compose message' form with predefined information
     function composeMail() {
         Office.context.mailbox.displayNewMessageForm({
-            toRecipients: ["Jeroen.van.der.Velden2@hva.nl"],
+            toRecipients: [securityTeamMailAddress],
             // ccRecipients: ["sam@contoso.com"], Send to more mailaddresses if nessecery
             subject: Office.context.mailbox.userProfile.displayName + " is reporting phishing!",
-            htmlBody: 'Hello <b>phishing</b>!<br/></i>' + phishItemId,
+            htmlBody: 'PhishItemId: ' + phishItemId + '<br>MailAddress: ' + securityTeamMailAddress,
             attachments: [
                 { type: "item", itemId: phishItemId, name: phishSubject }
             ],
--- PhishReporterWeb/MessageRead.js
+++ PhishReporterWeb/MessageRead.js
@@ -6,45 +6,32 @@
         // Office is ready
         $(document).ready(function () {
             // The document is ready
-            loadItemProps(Office.context.mailbox.item);
-            
-            composeMail();
+            loadCurrentMailAddress();  
         });
     });
 
-    
-
-    function loadItemProps(item) {
+    function loadCurrentMailAddress() {
         // Write message property values to the task pane
-        $('#item-id').text(item.itemId);
-        $('#item-subject').text(item.subject);
-        $('#item-internetMessageId').text(item.internetMessageId);
-        $('#item-from').html(item.from.displayName + " &lt;" + item.from.emailAddress + "&gt;");
-
-        
-
-        
-
-        //$('#debug').text(item.itemClass);
-
-       
-
-        //if (item.displayNewMessageForm !== undefined) {
-        //    $('#debug').text("displayNewMessageForm can be used.");
-            // Use item.displayNewMessageForm.
-        //    item.displayNewMessageForm;
-        //} else {
-        //    $('#debug').text("displayNewMessageForm can't be used.");
-        //}
-    }
-
-    function composeMail() {
-        Office.context.mailbox.displayNewMessageForm({
-            toRecipients: Office.context.mailbox.item.to, // Copies the To line from current item
-            ccRecipients: ["sam@contoso.com"],
-            subject: "Outlook add-ins are cool!",
-            htmlBody: 'Hello <b>World</b>!<br/><img src="cid:image.png"></i>',
-        });
+        document.getElementById("currentMailAddress").innerHTML = Office.context.roamingSettings.get("email");
     }
 
 })();
+
+function changeMailAddress() {
+    var newMailAddress = document.getElementById("newMailAddress").value;
+    Office.context.roamingSettings.set("email", newMailAddress);
+    saveRoamingSettings();
+}
+// TODO: visual feedback als mail gewijzigd is.
+
+function saveRoamingSettings() {
+    // Save settings in the mailbox to make it available in future sessions.
+    Office.context.roamingSettings.saveAsync(function (result) {
+        if (result.status !== Office.AsyncResultStatus.Succeeded) {
+            console.error(`Action failed with message ${result.error.message}`);
+        } else {
+            console.log(`Settings saved with status: ${result.status}`);
+        }
+    });
+}
+
