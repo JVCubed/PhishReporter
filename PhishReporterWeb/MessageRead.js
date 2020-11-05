f88408f70335037d9ffa545711ba0bd844eb46e8
JVCubed <JVCubed@outlook.com>
2020-11-06 00:40:40 +0100

    Modified 'how to detect phishing' added the 10 options about how to detect and changed the style a bit.

--- PhishReporterWeb/MessageRead.js
+++ PhishReporterWeb/MessageRead.js
@@ -4,25 +4,26 @@
 
     Office.onReady(function () {
         // Office is ready
+       
+
         $(document).ready(function () {
             // The document is ready
             loadCurrentMailAddress();  
         });
     });
 
-    function loadCurrentMailAddress() {
-        // Write message property values to the task pane
-        document.getElementById("currentMailAddress").innerHTML = Office.context.roamingSettings.get("email");
-    }
-
 })();
+
+function loadCurrentMailAddress() {
+    // Write message property values to the task pane
+    document.getElementById("currentMailAddress").innerHTML = Office.context.roamingSettings.get("email");
+}
 
 function changeMailAddress() {
     var newMailAddress = document.getElementById("newMailAddress").value;
     Office.context.roamingSettings.set("email", newMailAddress);
     saveRoamingSettings();
 }
-// TODO: visual feedback als mail gewijzigd is.
 
 function saveRoamingSettings() {
     // Save settings in the mailbox to make it available in future sessions.
@@ -31,7 +32,7 @@ function saveRoamingSettings() {
             console.error(`Action failed with message ${result.error.message}`);
         } else {
             console.log(`Settings saved with status: ${result.status}`);
+            loadCurrentMailAddress()
         }
     });
 }
-
